fa4d23d87c8f2bfb67e7d3b0fc1e7e7aaca63d3a
Gustaf Erikson <gerikson@gmail.com>
2022-12-04 23:23:01 +0100

    formatting cleanup

--- d04-Camp-Cleanup.pl
+++ d04-Camp-Cleanup.pl
@@ -64,12 +64,8 @@ say sec_to_hms( tv_interval($start_time) );
 ### SUBS
 sub sec_to_hms {
     my ($s) = @_;
-    return sprintf(
-        "Duration: %02dh%02dm%02ds (%.3f ms)",
-        int( $s / ( 60 * 60 ) ),
-        ( $s / 60 ) % 60,
-        $s % 60, $s * 1000
-    );
+    return sprintf("Duration: %02dh%02dm%02ds (%.3f ms)",
+        int( $s / ( 60 * 60 ) ),( $s / 60 ) % 60,$s % 60, $s * 1000);
 }
 
 ###########################################################

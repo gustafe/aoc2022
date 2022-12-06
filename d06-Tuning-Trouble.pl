ab968bdf94cf0a2d4f6b0185af899d51d9ebe1d9
Gustaf Erikson <gerikson@gmail.com>
2022-12-06 07:18:00 +0100

    updated score and rating

--- d06-Tuning-Trouble.pl
+++ d06-Tuning-Trouble.pl
@@ -78,5 +78,9 @@ parametrizing the window size, because that made part 2 trivial. That
 said my finishing standings were in the 16,000s despite a personal
 best finishing time this year.
 
+Score: 2
+
+Puzzle rating: 3/5, too easy to make this really interesting.
+
 =cut
 

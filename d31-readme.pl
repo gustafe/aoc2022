6054376c01a908788ac34df22077596bf6a9fc46
Gustaf Erikson <gerikson@gmail.com>
2022-12-20 10:48:56 +0100

    added todo list

--- d31-readme.pl
+++ d31-readme.pl
@@ -13,6 +13,20 @@ I use Perl for all the solutions.
 Most assume the input data is in a file called C<input.txt> in the same
 directory as the file.
 
+=head2 TODO
+
+=over
+
+=item complete day 16
+
+=item complete day 17
+
+=item complete day 19
+
+=item publish day 20
+
+=back
+
 =head2 Solution comments in reverse order
 
 =cut
